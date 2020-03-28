af98b64f281759ae4f44658b182c78d59d363c02
Chinh Le <chinh.le11@gmail.com>
2020-03-28 11:47:31 -0400

    Add markdown and react html parser

--- src/MarkdownComponent.js
+++ src/MarkdownComponent.js
@@ -1,37 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Container, Row, Col, Accordion, Card,
 } from 'react-bootstrap';
+import MarkdownIt from 'markdown-it';
+import ReactHtmlParser from 'react-html-parser';
 
 import './custom.scss';
 
+const MarkdownComponent = () => {
+  const [mark, setMark] = useState('');
 
-const MarkdownComponent = () => (
-  <Container fluid>
-    <Row><h1>Markdown Previewer</h1></Row>
-    <Row>
-      <Col sm={12} md={6}>
-        <Accordion defaultActiveKey="0">
-          <Card>
-            <Card.Header>Editor</Card.Header>
-            <Card.Body>
-              <textarea />
-            </Card.Body>
-          </Card>
-        </Accordion>
-      </Col>
-      <Col sm={12} md={6}>
-        <Accordion defaultActiveKey="0">
-          <Card>
-            <Card.Header>Previewer</Card.Header>
-            <Card.Body>
-              <Card.Text />
-            </Card.Body>
-          </Card>
-        </Accordion>
-      </Col>
-    </Row>
-  </Container>
-);
+  const md = new MarkdownIt();
+
+  const changeHandler = (evt) => {
+    setMark(evt.target.value);
+  };
+
+  return (
+    <Container fluid>
+      <h1>Markdown Previewer</h1>
+      <Row>
+        <Col sm={12} md={6} lg={4}>
+          <Accordion defaultActiveKey="0">
+            <Card>
+              <Card.Header>Editor</Card.Header>
+              <Card.Body>
+                <textarea onChange={changeHandler} />
+              </Card.Body>
+            </Card>
+          </Accordion>
+        </Col>
+        <Col sm={12} md={6} lg={4}>
+          <Accordion defaultActiveKey="0">
+            <Card>
+              <Card.Header>Preview</Card.Header>
+              <Card.Body>
+                {ReactHtmlParser(md.render(mark))}
+              </Card.Body>
+            </Card>
+          </Accordion>
+        </Col>
+        <Col sm={12} md={6} lg={4}>
+          <Accordion defaultActiveKey="0">
+            <Card>
+              <Card.Header>HTML</Card.Header>
+              <Card.Body>
+                {md.render(mark)}
+              </Card.Body>
+            </Card>
+          </Accordion>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default MarkdownComponent;
