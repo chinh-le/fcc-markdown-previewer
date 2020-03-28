44c1e6aa540bc0111f0607763e89d1a0e021e3c7
Chinh Le <chinh.le11@gmail.com>
2020-03-28 15:23:02 -0400

    Add style

--- src/MarkdownComponent.js
+++ src/MarkdownComponent.js
@@ -5,12 +5,16 @@ import {
 import MarkdownIt from 'markdown-it';
 import ReactHtmlParser from 'react-html-parser';
 
-import './custom.scss';
+// import 'bootstrap/dist/css/bootstrap.min.css'; // not required if using Sass
+import './MarkdownComponent.scss';
+
+import { MD } from './CONST';
 
 const MarkdownComponent = () => {
-  const [mark, setMark] = useState('');
+  const initialMds = MD.join('\n');
+  const [mark, setMark] = useState(initialMds);
 
-  const md = new MarkdownIt();
+  const mdi = new MarkdownIt();
 
   const changeHandler = (evt) => {
     setMark(evt.target.value);
@@ -20,32 +24,30 @@ const MarkdownComponent = () => {
     <Container fluid>
       <h1>Markdown Previewer</h1>
       <Row>
-        <Col sm={12} md={6} lg={4}>
-          <Accordion defaultActiveKey="0">
+        <Col sm={12} md={6} className="mb-4">
+          <Accordion className="mb-4" defaultActiveKey="0">
             <Card>
               <Card.Header>Editor</Card.Header>
               <Card.Body>
-                <textarea onChange={changeHandler} />
+                <textarea onChange={changeHandler} className="w-100 border-0" rows="10" placeholder="Mark it Down!" id="editor" />
               </Card.Body>
             </Card>
           </Accordion>
-        </Col>
-        <Col sm={12} md={6} lg={4}>
-          <Accordion defaultActiveKey="0">
-            <Card>
-              <Card.Header>Preview</Card.Header>
-              <Card.Body>
-                {ReactHtmlParser(md.render(mark))}
-              </Card.Body>
-            </Card>
-          </Accordion>
-        </Col>
-        <Col sm={12} md={6} lg={4}>
           <Accordion defaultActiveKey="0">
             <Card>
               <Card.Header>HTML</Card.Header>
               <Card.Body>
-                {md.render(mark)}
+                {mdi.render(mark)}
+              </Card.Body>
+            </Card>
+          </Accordion>
+        </Col>
+        <Col sm={12} md={6} className="mb-4">
+          <Accordion defaultActiveKey="0">
+            <Card id="preview">
+              <Card.Header>Preview</Card.Header>
+              <Card.Body>
+                {ReactHtmlParser(mdi.render(mark))}
               </Card.Body>
             </Card>
           </Accordion>
