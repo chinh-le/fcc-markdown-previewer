50a478289d862bcbd8ed2cc0750bfc7531058a87
Chinh Le <chinh.le11@gmail.com>
2020-03-28 20:38:24 -0400

    Add Google font Roboto

--- src/components/HtmlComponent.js
+++ src/components/HtmlComponent.js
@@ -6,7 +6,7 @@ const HtmlComponent = (props) => {
     return (
         <Accordion defaultActiveKey="0">
             <Card>
-              <Accordion.Toggle as={Card.Header} className="font-weight-bold mb-0" eventKey="1">HTML</Accordion.Toggle>
+              <Accordion.Toggle as={Card.Header} className="font-weight-bold mb-0" style={{fontFamily: 'Roboto, Helvetica, sans-serif'}} eventKey="1">HTML</Accordion.Toggle>
               <Accordion.Collapse eventKey="1">
                 <Card.Body className="bg-light"><pre>{props.mark}</pre></Card.Body>
               </Accordion.Collapse>
